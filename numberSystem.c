40ae2ee6171f3fdc690b64c6515555f70f608fae
Aljoharah-Aljubair <aljohara.jub@gmail.com>
2022-12-07 01:27:00 +0300

    number system

---
+++ numberSystem.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main(){  
+    int t = 10;
+    printf("10 to octal = %o \n",t );
+    printf("10 to hex Lower = %x \n",t ); 
+    printf("10 to hex Upper = %X \n",t ); 
+    
+    return 0;   
+}  
+
